2ca48e7f37ee0bb265820ab8b55657d14c1df7df
Shashank <shashank.naik@data-axle.com>
2022-07-20 13:32:19 +0530

    Initial commit.

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Blogs from './components/Blogs';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>List of blogs</h1>
+      <Blogs />
+    </div>
+  );
+}
+
+export default App;
